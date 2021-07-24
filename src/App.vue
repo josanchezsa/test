b8f4656ec4033ca5ef43de5e2910dacd27b58aee
Jose Sanchez <jsanchezs2@est.ups.edu.ec>
2021-07-24 13:45:24 -0500

    instalaciones

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
-<template>
-  <div id="app">
-    <img alt="Torre Logo" src="./assets/icons/torre.png">
-    <ShowData/>
-  </div>
+<template lang="pug">
+  v-app
+    v-app-bar(app='' color='primary' dark='')
+      .d-flex.align-center
+        v-img.shrink.mr-2(alt='Vuetify Logo' contain='' src='./assets/icons/torre.png' transition='scale-transition' width='40')
+        p Hola
+        v-img.shrink.mt-1.hidden-sm-and-down(alt='Vuetify Name' contain='' min-width='100' src='https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png' width='100')
+      v-spacer
+      v-btn(href='https://github.com/vuetifyjs/vuetify/releases/latest' target='_blank' text='')
+        span.mr-2 Latest Release
+        v-icon mdi-open-in-new
+    v-main
+      showData
 </template>
 
 <script>
-import ShowData from './components/ShowData.vue'
+import ShowData from './components/ShowData.vue';
 
 export default {
   name: 'App',
+
   components: {
     ShowData
-  }
-}
-</script>
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  publicPath: '/test/'
+  publicPath: '/test/',
+
+  transpileDependencies: [
+    'vuetify'
+  ]
 }
